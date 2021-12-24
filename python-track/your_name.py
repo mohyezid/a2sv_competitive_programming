594bd2f297a06d04f90537cebf1d5d42cb74f033
Mohammed yezid <88721945+ibnuyezid@users.noreply.github.com>
2021-12-24 23:26:30 +0300

    Create your_name.py

---
+++ python-track/your_name.py
@@ -0,0 +1,11 @@
+def print_full_name(first, last):
+    str="Hello "+first+" "+last+"!"+" You just delved into python."
+    print( str)
+    
+    
+   
+
+if __name__ == '__main__':
+    first_name = raw_input()
+    last_name = raw_input()
+    print_full_name(first_name, last_name)
