293cf883ea4fd7c42b8996bef87c2db3697c3ce0
Mohammed yezid <88721945+ibnuyezid@users.noreply.github.com>
2022-02-26 15:43:55 +0300

    Create cycle_Detection.py

---
+++ week3/cycle_Detection.py
@@ -0,0 +1,57 @@
+
+# Complete the has_cycle function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def has_cycle(head):
+    s=set()
+    
+    curr=head
+    
+    while curr!=None:
+        if curr.next  in s:
+            return 1
+        else:
+            s.add(curr.next)
+        curr=curr.next
+    return 0
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        index = int(input())
+
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        extra = SinglyLinkedListNode(-1);
+        temp = llist.head;
+
+        for i in range(llist_count):
+            if i == index:
+                extra = temp
+
+            if i != llist_count-1:
+                temp = temp.next
+
+        temp.next = extra
+
+        result = has_cycle(llist.head)
+
+        fptr.write(str(int(result)) + '\n')
+
+    fptr.close()
